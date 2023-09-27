eff7f197650627dbf1106bc60e2098def5fa589f
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-09-27 20:29:23 -0300

    feat: comentarios para validações

--- app/components/Inicio/index.tsx
+++ app/components/Inicio/index.tsx
@@ -1,21 +1,27 @@
-import Image from 'next/image'
-import inicioLogo from '../../../public/inicio.svg'
-import styles from './style.module.css'
+import Image from 'next/image';
+import inicioLogo from '../../../public/inicio.svg';
+import styles from './style.module.css';
 
+// modo de exportação
 
 export function Inicio() {
-    return (
-        <>
-            <div className={styles.container}>
-                <div className={styles.text}>
-                    <h1 className={styles.title}>Impulsionamos pessoas, carreiras e projetos sociais</h1>
-                    <p className={styles.description}>Somos uma comunidade de profissionais voluntários e acreditamos que a tecnologia é um meio poderoso para transformar vidas.</p>
-                </div>
-                <Image src={inicioLogo} alt='Logo Inicio' />
-            </div>
-            <div className={styles.testeQuemSomos}>
-
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className={styles.container}>
+        <div className={styles.text}>
+          <h1 className={styles.title}>
+            Impulsionamos pessoas, carreiras e projetos sociais
+          </h1>
+          <p className={styles.description}>
+            Somos uma comunidade de profissionais voluntários e acreditamos que
+            a tecnologia é um meio poderoso para transformar vidas.
+          </p>
+        </div>
+        <Image src={inicioLogo} alt='Logo Inicio' />
+        {/* onde salvar as imagens, qual alt utilizar */}
+      </div>
+      <div className={styles.testeQuemSomos}></div>
+      {/* verificar se vai manter essa div ou juntar com a proxima sessão */}
+    </>
+  );
 }
