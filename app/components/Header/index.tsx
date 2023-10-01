548a3d48dccf1fd99f4a3822b4da4de96123f107
William Firmino <williamwf07@gmail.com>
2023-10-01 03:27:46 -0300

    feat: Change of color variables from style guide Add div for menu background opacity add margin behind the header
    Co-authored-by: Ronan Salvador <ronansalvador@yahoo.com.br>

--- app/components/Header/index.tsx
+++ app/components/Header/index.tsx
@@ -1,91 +1,110 @@
-"use client";
+'use client'
 
-import styles from "./styles.module.css";
-import Link from "next/link";
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import styles from './styles.module.css'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
 
-import logo from "../../../public/logo1.svg";
+import logo from '../../../public/logo1.svg'
 
 export function Header() {
-  const [isMenuOpened, setIsMenuOpened] = useState<boolean>(false);
-  const [headerHeight, setHeaderHeight] = useState<number>();
-  const [hasShadow, setHasShadow] = useState<boolean>(false);
+  const [isMenuOpened, setIsMenuOpened] = useState<boolean>(false)
+  const [headerHeight, setHeaderHeight] = useState<number>()
+  const [hasShadow, setHasShadow] = useState<boolean>(false)
 
   useEffect(() => {
     const updateHeaderHeight = () => {
-      const headerElement = document.querySelector("#header");
+      const headerElement = document.querySelector('#header')
       if (headerElement) {
-        setHeaderHeight(headerElement.clientHeight);
+        setHeaderHeight(headerElement.clientHeight)
       }
-    };
+    }
 
-    updateHeaderHeight();
-  });
+    updateHeaderHeight()
+  })
 
   useEffect(() => {
     function checkScroll() {
       if (window.scrollY > 0) {
-        setHasShadow(true);
+        setHasShadow(true)
       } else {
-        setHasShadow(false);
+        setHasShadow(false)
       }
     }
 
-    window.addEventListener("scroll", checkScroll);
+    window.addEventListener('scroll', checkScroll)
 
-    checkScroll();
+    checkScroll()
 
     return () => {
-      window.removeEventListener("scroll", checkScroll);
-    };
-  }, []);
+      window.removeEventListener('scroll', checkScroll)
+    }
+  }, [])
 
   return (
-    <header
-      id="header"
-      className={`${styles.header} ${hasShadow ? styles.hasShadow : ""}`}
-    >
-      <div className={styles.headerContainer}>
-        <Link className={styles.logo} href="/">
-          <Image
-            className={styles.logo}
-            src={logo}
-            alt="Logo da Tech Pro Bem"
-          />
-        </Link>
+    <>
+      <header
+        id="header"
+        className={`${styles.header} ${hasShadow ? styles.hasShadow : ''}`}
+      >
+        <div className={styles.headerContainer}>
+          <Link className={styles.logo} href="/">
+            <Image
+              className={styles.logo}
+              src={logo}
+              alt="Logo da Tech Pro Bem"
+            />
+          </Link>
 
-        <button
-          className={styles.menuButton}
-          onClick={() => setIsMenuOpened(!isMenuOpened)}
-          type="button"
-        >
-          <div
-            className={`${styles.hamburguerIcon} ${
-              isMenuOpened ? styles.closeIcon : ""
-            }`}
+          <button
+            className={styles.menuButton}
+            onClick={() => setIsMenuOpened(!isMenuOpened)}
+            type="button"
           >
-            <span></span>
-            <span></span>
-            <span></span>
-          </div>
-        </button>
+            <div
+              className={`${styles.hamburguerIcon} ${
+                isMenuOpened ? styles.closeIcon : ''
+              }`}
+            >
+              <span></span>
+              <span></span>
+              <span></span>
+            </div>
+          </button>
 
-        <nav
-          className={`${styles.navbar} ${isMenuOpened ? styles.open : ""}`}
-          style={{ top: `${headerHeight}px` }}
-        >
-          <div>
-            <Link href="">Início</Link>
-            <Link href="">Sobre</Link>
-            <Link href="">Impacto</Link>
-            <Link href="">Depoimentos</Link>
-            <Link href="">Dúvidas</Link>
-            <Link href="">Nos acompanhe</Link>
-            <Link href="">Contato</Link>
-          </div>
-        </nav>
-      </div>
-    </header>
-  );
+          <nav
+            className={`${styles.navbar} ${isMenuOpened ? styles.open : ''}`}
+            style={{ top: `${headerHeight}px` }}
+          >
+            <ul className="body1">
+              <li>
+                <Link href="">Início</Link>
+              </li>
+              <li>
+                <Link href="">Sobre</Link>
+              </li>
+              <li>
+                <Link href="">Impacto</Link>
+              </li>
+              <li>
+                <Link href="">Depoimentos</Link>
+              </li>
+              <li>
+                <Link href="">Dúvidas</Link>
+              </li>
+              <li>
+                <Link href="">Nos acompanhe</Link>
+              </li>
+              <li>
+                <Link href="">Contato</Link>
+              </li>
+            </ul>
+
+            <div className={styles.opacityMenu}></div>
+          </nav>
+        </div>
+      </header>
+      <div style={{ marginBottom: `${headerHeight}px` }}></div>
+    </>
+  )
 }
