259da68136c7c56ce3b7e9519cd25b508ffc6047
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-10-19 14:28:50 -0300

    refactor(refact): quem somos ronan

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { SessionBegin } from './components/SessionBegin'
+import { QuemSomos } from './components/quemsomos'
 
 export default function Home() {
   return (
     <main>
       <SessionBegin />
+      <QuemSomos />
     </main>
   )
 }
