e8db923466964f1072ff12eb69b1e2940cf3961b
RubsRafa <rubiarafaela2000@gmail.com>
2023-10-19 15:18:05 -0300

    feat: our values component with responsive style

---
+++ app/components/OurValuesSection/index.tsx
@@ -0,0 +1,42 @@
+'use client'
+
+import { Tables, Values } from '@/app/protocols'
+import { getContentfulData } from '@/app/utils/getContentfulData'
+import Image from 'next/image'
+import { useEffect, useState } from 'react'
+import style from './style.module.css'
+
+export function OurValues() {
+  const [values, setValues] = useState<Tables<Values>[]>([])
+  useEffect(() => {
+    async function getData() {
+      const data: Tables<Values>[] = await getContentfulData('ourValues')
+      setValues(data)
+    }
+    getData()
+  }, [])
+  return (
+    <section className={style.main_box}>
+      <div className={style.values}>
+        <h3 className={`h3 ${style.title}`}>Nossos Valores</h3>
+        <div className={style.values_content}>
+          {values?.map((value: Tables<Values>) => (
+            <div key={value.fields.title} className={style.value}>
+              <Image
+                className={style.value_image}
+                src={`https:${value.fields.symbol.fields.file.url}`}
+                alt={value.fields.title}
+                width={value.fields.symbol.fields.file.details.image.width}
+                height={value.fields.symbol.fields.file.details.image.height}
+              ></Image>
+              <h4 className={`h4 ${style.value_text}`}>{value.fields.title}</h4>
+              <span className={style.value_description}>
+                {value.fields.description}
+              </span>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  )
+}
