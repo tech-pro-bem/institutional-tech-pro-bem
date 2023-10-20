773906cca6a050499426ca23fcbe5c910a1b2d77
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-10-20 18:07:53 -0300

    feat: remove section begin

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
 import { SessionAboutUs } from './components/SessionAboutUs'
-import { SessionBegin } from './components/SessionBegin'
 
 export default function Home() {
   return (
     <main>
-      <SessionBegin />
       <SessionAboutUs />
     </main>
   )
