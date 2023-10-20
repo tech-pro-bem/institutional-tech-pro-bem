1103d04502a81c949d1e6acd24e1d891434be03e
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-10-20 18:24:57 -0300

    feat: insert session begin

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 import { SessionAboutUs } from './components/SessionAboutUs'
+import { SessionBegin } from './components/SessionBegin'
 
 export default function Home() {
   return (
     <main>
+      <SessionBegin />
       <SessionAboutUs />
     </main>
   )
