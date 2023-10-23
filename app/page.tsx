a8dc3d22037f73f256e39bc05b40ff717ff524b8
RubsRafa <rubiarafaela2000@gmail.com>
2023-10-23 19:56:15 -0300

    fix: addressing Prettier errors for deploy

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import { OurValues } from './components/OurValuesSection'
-import { Header } from "./components/Header";
-import { Inicio } from "./components/Inicio";
+import { Header } from './components/Header'
+import { Inicio } from './components/Inicio'
 export default function Home() {
   return (
     <>
