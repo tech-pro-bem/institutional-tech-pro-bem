34820001d69287fb47e8d61a22e65fea2f00910f
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-10-26 19:59:37 -0300

    feat(cypress): teste conteudo

--- cypress/e2e/about.cy.ts
+++ cypress/e2e/about.cy.ts
@@ -4,12 +4,23 @@ describe('session about us', () => {
     cy.wait(3000)
   })
 
-  it('title Quem somos', () => {
-    cy.viewport(1935, 1080)
+  it('verifica se a classe about existe', () => {
     cy.get('[class^="style_about"]').should('exist')
+  })
+
+  it('title Quem somos e subtitulo como funciona', () => {
     cy.get('h2').contains('Quem somos')
     cy.get('h3').contains('Como funciona?')
+    cy.get('[class^="style_right"]')
+      .find(':first-child') // Seleciona o primeiro elemento filho da div.text-right
+      .should(
+        'have.text',
+        'A Tech Pro Bem é uma comunidade de tecnologia onde conectamos profissionais de diferentes áreas para criar soluções digitais para ONGs e projetos sociais.',
+      ) // Substitua 'Texto Esperado' pelo conteúdo esperado
+  })
 
+  it('tamanho section about', () => {
+    cy.viewport(1935, 1080)
     cy.get('[class^="style_about"]').then(($elemento) => {
       // Obtém as dimensões reais do elemento
       const larguraElemento = $elemento[0].clientWidth
@@ -17,7 +28,17 @@ describe('session about us', () => {
 
       // Realize as asserções para verificar as dimensões
       expect(larguraElemento).to.equal(1920) // px
-      expect(alturaElemento).to.equal(510) // 544
+      expect(alturaElemento).to.equal(685) // 544
+    })
+  })
+
+  it('verifica container', () => {
+    cy.viewport(1920, 1080)
+
+    cy.get('.container').should('exist')
+    cy.get('.container').then(($elemento) => {
+      const larguraElemento = $elemento[1].clientWidth
+      expect(larguraElemento).to.equal(1170) // px
     })
   })
 })
