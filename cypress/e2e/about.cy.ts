f7961036594366bb1aef1bd7d7f98559ea7b49a3
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-10-26 19:31:02 -0300

    feat: teste dimensão

--- cypress/e2e/about.cy.ts
+++ cypress/e2e/about.cy.ts
@@ -5,8 +5,19 @@ describe('session about us', () => {
   })
 
   it('title Quem somos', () => {
-    cy.viewport(1280, 1080)
+    cy.viewport(1935, 1080)
+    cy.get('[class^="style_about"]').should('exist')
     cy.get('h2').contains('Quem somos')
     cy.get('h3').contains('Como funciona?')
+
+    cy.get('[class^="style_about"]').then(($elemento) => {
+      // Obtém as dimensões reais do elemento
+      const larguraElemento = $elemento[0].clientWidth
+      const alturaElemento = $elemento[0].clientHeight
+
+      // Realize as asserções para verificar as dimensões
+      expect(larguraElemento).to.equal(1920) // px
+      expect(alturaElemento).to.equal(510) // 544
+    })
   })
 })
