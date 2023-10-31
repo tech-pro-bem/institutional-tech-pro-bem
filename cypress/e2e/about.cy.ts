6364ae33185b4fb21bbced62514fc8c0dce037ab
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-10-31 15:15:39 -0300

    feat(teste): teste

--- cypress/e2e/about.cy.ts
+++ cypress/e2e/about.cy.ts
@@ -12,13 +12,13 @@ describe('session about us', () => {
     cy.get('h3').contains('Como funciona?')
   })
   it('should have the text on the left', () => {
-    cy.get('[class^="style_left"]')
+    cy.get('[class^="style_text__block_1"]')
       .find(':first-child')
       .should(
         'have.text',
         'A Tech Pro Bem é uma comunidade de tecnologia onde conectamos profissionais de diferentes áreas para criar soluções digitais para ONGs e projetos sociais.',
       )
-    cy.get('[class^="style_left"]')
+    cy.get('[class^="style_text__block_1"]')
       .find(':nth-child(2)')
       .should(
         'have.text',
@@ -26,11 +26,21 @@ describe('session about us', () => {
       )
   })
   it('should have the text on the right', () => {
-    cy.get('[class^="style_right"]')
+    cy.get('[class^="style_text__block_2"]')
       .find(':nth-child(2)')
       .contains(
         'Selecionamos uma organização sem fins lucrativos que entrou em contato conosco e passou por um alinhamento de objetivos e expectativas. A cada ciclo realizamos um processo seletivo para formar nossas equipes com',
       )
+  })
+
+  it('verifica padding da sessao about', () => {
+    cy.viewport(1935, 1080)
+    cy.get('[class^="style_about"]').should('have.css', 'padding-top', '130px')
+    cy.get('[class^="style_about"]').should(
+      'have.css',
+      'padding-bottom',
+      '232px',
+    )
   })
 
   it('should check height and width of section about', () => {
@@ -39,13 +49,13 @@ describe('session about us', () => {
       const larguraElemento = $elemento[0].clientWidth
       const alturaElemento = $elemento[0].clientHeight
       expect(larguraElemento).to.equal(1920)
-      expect(alturaElemento).to.equal(685)
+      expect(alturaElemento).to.equal(633)
     })
   })
 
   it('should check height and width of section about', () => {
     // Ajuste a viewport para o tamanho desejado
-    cy.viewport(1920, 1080)
+    cy.viewport(1935, 1080)
 
     // Selecione o elemento
     cy.get('[class^="style_about"]').then(($elemento) => {
@@ -60,7 +70,7 @@ describe('session about us', () => {
 
       // Verifique as dimensões do elemento
       expect(larguraElemento).to.equal(1920)
-      expect(alturaElemento).to.equal(685)
+      expect(alturaElemento).to.equal(633)
     })
   })
 
@@ -81,7 +91,7 @@ describe('should check overflow', () => {
     cy.get('[class^="style_about"]').then(($about) => {
       const aboutWidth = $about.width()
       const aboutHeight = $about.height()
-      cy.get('.container').then((container) => {
+      cy.get('[class^="style_layout_about"]').then((container) => {
         const containerWidth = container.width()
         const containerHeight = container.height()
         expect(containerWidth).to.be.at.most(Number(aboutWidth))
@@ -94,11 +104,11 @@ describe('should check overflow', () => {
 describe('should check overflow text', () => {
   it('verifica se o elemento filho está dentro dos limites do elemento pai', () => {
     cy.visit('http://localhost:3000')
-    cy.get('[class^="style_left"]').then(($left) => {
+    cy.get('[class^="style_text__block_1"]').then(($left) => {
       const leftWidth = $left.width()
       const leftHeight = $left.height()
 
-      cy.get('[class^="style_left"]')
+      cy.get('[class^="style_text__block_1"]')
         .find(':first-child')
         .then((firstParagraph) => {
           const firstParagraphWidth = firstParagraph.width()
@@ -107,7 +117,7 @@ describe('should check overflow text', () => {
           expect(firstParagraphHeight).to.be.at.most(Number(leftHeight))
         })
 
-      cy.get('[class^="style_left"]')
+      cy.get('[class^="style_text__block_1"]')
         .find(':nth-child(2)')
         .then((secondParagraph) => {
           const secondParagraphWidth = secondParagraph.width()
@@ -119,21 +129,21 @@ describe('should check overflow text', () => {
   })
 })
 
-describe('should check overflow text left', () => {
-  it('verifica se os 2 paragrafos entao dentro da div left', () => {
+describe('should check overflow text block 1', () => {
+  it('verifica se os 2 paragrafos entao dentro da div text__block_1', () => {
     cy.visit('http://localhost:3000')
-    cy.get('[class^="style_left"]').then(($pai) => {
+    cy.get('[class^="style_text__block_1"]').then(($pai) => {
       const paiWidth = $pai.width()
       const paiHeight = $pai.height()
 
-      cy.get('[class^="style_left"]')
+      cy.get('[class^="style_text__block_1"]')
         .find(':first-child')
         .then((filho) => {
           const filhoWidth = Number(filho.width())
           expect(filhoWidth).to.be.at.most(Number(paiWidth))
           const filhoHeight = Number(filho.height())
 
-          cy.get('[class^="style_left"]')
+          cy.get('[class^="style_text__block_1"]')
             .find(':nth-child(2)')
             .then((filho2) => {
               const filho2Width = Number(filho2.width())
