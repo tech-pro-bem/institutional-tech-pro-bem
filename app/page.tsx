642c11afe33372236e85eb9604fc74933bf621b7
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-10-31 21:35:18 -0300

    feat: add ourValues

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,8 @@ export default function Home() {
         <SessionBegin />
         <SessionAboutUs />
         <OurValues />
+        <OurValues />
+        <OurValues />
       </main>
     </>
   )
