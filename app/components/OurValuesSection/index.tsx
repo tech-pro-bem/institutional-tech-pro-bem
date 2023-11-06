ba0c4c536a70cf76308bf61964a22b3898cf0221
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-11-06 13:15:39 -0300

    feat: teste promisse all

--- app/components/OurValuesSection/index.tsx
+++ app/components/OurValuesSection/index.tsx
@@ -1,20 +1,24 @@
-'use client'
+// 'use client'
 
 import { Tables, Values } from '@/app/protocols'
-import { getContentfulData } from '@/app/utils/getContentfulData'
+// import { getContentfulData } from '@/app/utils/getContentfulData'
 import Image from 'next/image'
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 import style from './style.module.css'
 
-export function OurValues() {
-  const [values, setValues] = useState<Tables<Values>[]>([])
-  useEffect(() => {
-    async function getData() {
-      const data: Tables<Values>[] = await getContentfulData('ourValues')
-      setValues(data)
-    }
-    getData()
-  }, [])
+interface OurValuesProps {
+  values: Tables<Values>[] // Declara a propriedade values como um array de objetos Values
+}
+
+export const OurValues: React.FC<OurValuesProps> = async ({ values }) => {
+  // const [values, setValues] = useState<Tables<Values>[]>([])
+  // useEffect(() => {
+  //   async function getData() {
+  //     const data: Tables<Values>[] = await getContentfulData('ourValues')
+  //     setValues(data)
+  //   }
+  //   getData()
+  // }, [])
 
   // const values: Tables<Values>[] = await getContentfulData('ourValues')
 
--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,25 @@ import { OurValues } from './components/OurValuesSection'
 import { Header } from './components/Header'
 import { SessionBegin } from './components/SessionBegin'
 import { SessionAboutUs } from './components/SessionAboutUs'
+import { getContentfulData } from './utils/getContentfulData'
 
-export default function Home() {
+export default async function Home() {
+  const promises = [
+    getContentfulData('header'),
+    getContentfulData('begin'),
+    getContentfulData('about'),
+    getContentfulData('ourValues'),
+  ]
+
+  const [headerValues, beginValues, AboutUsValues, ourValues] =
+    await Promise.all(promises)
   return (
     <>
-      <Header />
+      <Header values={headerValues} />
       <main>
-        <SessionBegin />
-        <SessionAboutUs />
-        <OurValues />
+        <SessionBegin values={beginValues} />
+        <SessionAboutUs values={AboutUsValues} />
+        <OurValues values={ourValues} />
       </main>
     </>
   )
