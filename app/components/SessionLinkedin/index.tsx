b23b0c6fe41a15ef1149395da53dfe7800fbd13b
Bruna Santiago <brunasanthiago@outlook.com>
2023-12-05 10:36:14 -0300

    fix: bug style

--- app/components/SessionLinkedin/index.tsx
+++ app/components/SessionLinkedin/index.tsx
@@ -15,10 +15,13 @@ export const SessionLinkedin: React.FC<LinkedinProps> = async ({ values }) => {
   )
 
   return (
-    <section id="linkedin">
-      <div className={`${style.section__container_linkedin} container`}>
-        {values.map((value: Tables<LinkedinValues>) => (
-          <div className={style.section__container_text} key={value.fields.id}>
+    <section id="linkedin" className={style.section__container}>
+      {values.map((value: Tables<LinkedinValues>) => (
+        <div
+          key={value.fields.id}
+          className={`${style.section__container_linkedin} container`}
+        >
+          <div className={style.section__container_text}>
             <h2 className="title">{value.fields.title}</h2>
             <p
               className={`${style.text_paragraph} regular-text`}
@@ -28,7 +31,7 @@ export const SessionLinkedin: React.FC<LinkedinProps> = async ({ values }) => {
             />
             <div className={style.link}>
               <Link
-                href={`https://${value.fields.linkLinkedIn}`}
+                href={`https:${value.fields.linkLinkedIn}`}
                 className={style.link_linkedin}
               >
                 {value.fields.buttonText}
@@ -40,40 +43,16 @@ export const SessionLinkedin: React.FC<LinkedinProps> = async ({ values }) => {
                 height={value.fields.arrow.fields.file.details.image.height}
               />
             </div>
-            <Image
-              className={style.image__logo}
-              src={`https:${value.fields.image.fields.file.url}`}
-              alt={value.fields.image.title}
-              width={value.fields.image.fields.file.details.image.width}
-              height={value.fields.image.fields.file.details.image.height}
-            />
           </div>
-        ))}
-      </div>
+          <Image
+            className={style.image__logo}
+            src={`https:${value.fields.image.fields.file.url}`}
+            alt={value.fields.image.title}
+            width={value.fields.image.fields.file.details.image.width}
+            height={value.fields.image.fields.file.details.image.height}
+          />
+        </div>
+      ))}
     </section>
   )
 }
-
-// export function Linkedin() {
-//   return (
-//     <section className={style.section__container}>
-//       <div className={`${style.section__container_linkedin} container`}>
-//         <div className={style.section__container_text}>
-//           <h2 className="title">Acompanhe</h2>
-//           <p className="regular-text">
-//             Seguindo a Tech Pro Bem no <strong>Linkedin</strong> você fica
-//             sabendo do próximo processo seletivo e outras novidades. Nós vemos
-//             lá?
-//           </p>
-//           <div className={style.link}>
-//             <Link href="https://www.linkedin.com/company/techprobem/">
-//               Seguir a Tech Pro Bem
-//             </Link>
-//             <Image src={logo} alt="" />
-//           </div>
-//         </div>
-//         <Image src={linkedin} alt="" className={style.image__logo} />
-//       </div>
-//     </section>
-//   )
-// }
