ba410be229b94f63c760fc0457929ef9dadbed89
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-12-05 09:28:25 -0300

    refactor: new section depoiments

--- app/components/Depoiments/index.tsx
+++ app/components/Depoiments/index.tsx
@@ -3,16 +3,21 @@ import style from './style.module.css'
 import Image from 'next/image'
 
 import imgVoluntario from '../../../public/voluntario.png'
-import logo from '../../../public/logo1.svg'
 
 export const Depoiments = () => {
   return (
     <section className={`${style.depoiments}`}>
       <div className="container">
         <h1 className={`title ${style.title}`}>Depoimentos</h1>
+        <h2 className="title-small">
+          O que os voluntários que passaram pela Tech Pro Bem estão falando
+        </h2>
         <div className={style.grid}>
           <div className={style.card}>
-            <p className="regular-text">
+            <Image src={imgVoluntario} alt="imagem do voluntario" />
+            <p className="text2">Mirelle Lucena</p>
+            <p className={`text2 ${style.department}`}>Product Owner</p>
+            <p className={`regular-text ${style.text_depoiment}`}>
               “Ingressar na Tech Pro Bem foi um divisor de águas para mim. O
               voluntariado surgiu em um momento que estava precisando{' '}
               <strong>
@@ -22,62 +27,51 @@ export const Depoiments = () => {
               em time e ter cases práticos. Foi um estímulo e um motivador
               enquanto ainda não estava inserida no mercado de trabalho.”
             </p>
-            <div>
-              <Image src={imgVoluntario} alt="imagem do voluntario" />
-              <p className="text2">Mirelle Lucena</p>
-              <p className={`text2 ${style.department}`}>Product Owner</p>
-            </div>
           </div>
           <div className={style.card}>
-            <p className="regular-text">
-              “O trabalho voluntário na Tech Pro Bem está sendo enriquecedor por
-              me permitir colocar em prática os estudos em UX/UI Design,
-              trabalhar em equipe e ainda ajudar projetos sociais incriveis.
+            <Image src={imgVoluntario} alt="imagem do voluntario" />
+            <p className="text2">Mirelle Lucena</p>
+            <p className={`text2 ${style.department}`}>Product Owner</p>
+            <p className={`regular-text ${style.text_depoiment}`}>
+              “Ingressar na Tech Pro Bem foi um divisor de águas para mim. O
+              voluntariado surgiu em um momento que estava precisando{' '}
               <strong>
-                {' '}
-                Considero uma etapa fundamental na minha transição de carreira e
-                também no meu crescimento pessoal.{' '}
-              </strong>
-              Juntos, unimos forças e fazemos o bem!”
+                colocar muito do que tinha de conhecimento teórico em prática,
+              </strong>{' '}
+              estava buscando uma oportunidade para melhorar minha comunicação
+              em time e ter cases práticos. Foi um estímulo e um motivador
+              enquanto ainda não estava inserida no mercado de trabalho.”
             </p>
-            <div>
-              <Image src={imgVoluntario} alt="imagem do voluntario" />
-              <p className="text2">Mirelle Lucena</p>
-              <p className={`text2 ${style.department}`}>Product Owner</p>
-            </div>
           </div>
           <div className={style.card}>
-            <p className="regular-text">
-              “A Tech contribuiu para que meu conhecimento teórico se
-              transformasse em prática.
+            <Image src={imgVoluntario} alt="imagem do voluntario" />
+            <p className="text2">Mirelle Lucena</p>
+            <p className={`text2 ${style.department}`}>Product Owner</p>
+            <p className={`regular-text ${style.text_depoiment}`}>
+              “Ingressar na Tech Pro Bem foi um divisor de águas para mim. O
+              voluntariado surgiu em um momento que estava precisando{' '}
               <strong>
-                Pude ter contato com uma equipe real e com profissionais da área{' '}
-              </strong>
-              ao atuar num time ágil como QA Jr voluntária. Desenvolvi muito
-              minhas habilidades analíticas e de comunicação. Sou muito grata a
-              toda equipe por me proporcionar crescer como pessoa e como
-              profissional. Estou muito feliz e grata por todo aprendizado.” .”
+                colocar muito do que tinha de conhecimento teórico em prática,
+              </strong>{' '}
+              estava buscando uma oportunidade para melhorar minha comunicação
+              em time e ter cases práticos. Foi um estímulo e um motivador
+              enquanto ainda não estava inserida no mercado de trabalho.”
             </p>
-            <div>
-              <Image src={imgVoluntario} alt="imagem do voluntario" />
-              <p className="text2">Mirelle Lucena</p>
-              <p className={`text2 ${style.department}`}>Product Owner</p>
-            </div>
           </div>
           <div className={style.card}>
-            <p className="regular-text">
-              “Ser voluntário na Tech Pro Bem é um aprendizado incomparável!
-              <strong>Nenhum curso no mundo supera essa experiência</strong>
-              .”
+            <Image src={imgVoluntario} alt="imagem do voluntario" />
+            <p className="text2">Mirelle Lucena</p>
+            <p className={`text2 ${style.department}`}>Product Owner</p>
+            <p className={`regular-text ${style.text_depoiment}`}>
+              “Ingressar na Tech Pro Bem foi um divisor de águas para mim. O
+              voluntariado surgiu em um momento que estava precisando{' '}
+              <strong>
+                colocar muito do que tinha de conhecimento teórico em prática,
+              </strong>{' '}
+              estava buscando uma oportunidade para melhorar minha comunicação
+              em time e ter cases práticos. Foi um estímulo e um motivador
+              enquanto ainda não estava inserida no mercado de trabalho.”
             </p>
-            <div>
-              <Image src={imgVoluntario} alt="imagem do voluntario" />
-              <p className="text2">Mirelle Lucena</p>
-              <p className={`text2 ${style.department}`}>Product Owner</p>
-            </div>
-          </div>
-          <div className={style.card}>
-            <Image src={logo} alt="logo" />
           </div>
         </div>
       </div>
