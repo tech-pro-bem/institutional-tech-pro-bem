ea5ca238073fe5b3b535896679c4a699eebbfe19
Ronan Salvador <ronansalvador@yahoo.com.br>
2023-12-06 12:33:57 -0300

    feat: teste carousel

--- app/components/Depoiments/index.tsx
+++ app/components/Depoiments/index.tsx
@@ -1,80 +1,53 @@
-import React from 'react'
-import style from './style.module.css'
+'use client'
+import { register } from 'swiper/element/bundle'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
+import imgVoluntario from '../../../public/voluntario.png'
+
+import { Swiper, SwiperSlide } from 'swiper/react'
 import Image from 'next/image'
 
-import imgVoluntario from '../../../public/voluntario.png'
+register()
+
+const conteudo = [
+  {
+    id: 1,
+    nome: 'nome',
+    cargo: 'cargo',
+    image: '../../../public/voluntario.png',
+    depoimento: 'texto do depoimento',
+  },
+  {
+    id: 2,
+    nome: 'nome',
+    cargo: 'cargo',
+    image: '../../../public/voluntario.png',
+    depoimento: 'texto do depoimento',
+  },
+  {
+    id: 3,
+    nome: 'nome',
+    cargo: 'cargo',
+    image: '../../../public/voluntario.png',
+    depoimento: 'texto do depoimento',
+  },
+]
 
 export const Depoiments = () => {
   return (
-    <section className={`${style.depoiments}`}>
-      <div className="container">
-        <h1 className={`title ${style.title}`}>Depoimentos</h1>
-        <h2 className="title-small">
-          O que os voluntários que passaram pela Tech Pro Bem estão falando
-        </h2>
-        <div className={style.grid}>
-          <div className={style.card}>
-            <Image src={imgVoluntario} alt="imagem do voluntario" />
-            <p className="text2">Mirelle Lucena</p>
-            <p className={`text2 ${style.department}`}>Product Owner</p>
-            <p className={`regular-text ${style.text_depoiment}`}>
-              “Ingressar na Tech Pro Bem foi um divisor de águas para mim. O
-              voluntariado surgiu em um momento que estava precisando{' '}
-              <strong>
-                colocar muito do que tinha de conhecimento teórico em prática,
-              </strong>{' '}
-              estava buscando uma oportunidade para melhorar minha comunicação
-              em time e ter cases práticos. Foi um estímulo e um motivador
-              enquanto ainda não estava inserida no mercado de trabalho.”
-            </p>
-          </div>
-          <div className={style.card}>
-            <Image src={imgVoluntario} alt="imagem do voluntario" />
-            <p className="text2">Mirelle Lucena</p>
-            <p className={`text2 ${style.department}`}>Product Owner</p>
-            <p className={`regular-text ${style.text_depoiment}`}>
-              “Ingressar na Tech Pro Bem foi um divisor de águas para mim. O
-              voluntariado surgiu em um momento que estava precisando{' '}
-              <strong>
-                colocar muito do que tinha de conhecimento teórico em prática,
-              </strong>{' '}
-              estava buscando uma oportunidade para melhorar minha comunicação
-              em time e ter cases práticos. Foi um estímulo e um motivador
-              enquanto ainda não estava inserida no mercado de trabalho.”
-            </p>
-          </div>
-          <div className={style.card}>
-            <Image src={imgVoluntario} alt="imagem do voluntario" />
-            <p className="text2">Mirelle Lucena</p>
-            <p className={`text2 ${style.department}`}>Product Owner</p>
-            <p className={`regular-text ${style.text_depoiment}`}>
-              “Ingressar na Tech Pro Bem foi um divisor de águas para mim. O
-              voluntariado surgiu em um momento que estava precisando{' '}
-              <strong>
-                colocar muito do que tinha de conhecimento teórico em prática,
-              </strong>{' '}
-              estava buscando uma oportunidade para melhorar minha comunicação
-              em time e ter cases práticos. Foi um estímulo e um motivador
-              enquanto ainda não estava inserida no mercado de trabalho.”
-            </p>
-          </div>
-          <div className={style.card}>
-            <Image src={imgVoluntario} alt="imagem do voluntario" />
-            <p className="text2">Mirelle Lucena</p>
-            <p className={`text2 ${style.department}`}>Product Owner</p>
-            <p className={`regular-text ${style.text_depoiment}`}>
-              “Ingressar na Tech Pro Bem foi um divisor de águas para mim. O
-              voluntariado surgiu em um momento que estava precisando{' '}
-              <strong>
-                colocar muito do que tinha de conhecimento teórico em prática,
-              </strong>{' '}
-              estava buscando uma oportunidade para melhorar minha comunicação
-              em time e ter cases práticos. Foi um estímulo e um motivador
-              enquanto ainda não estava inserida no mercado de trabalho.”
-            </p>
-          </div>
-        </div>
-      </div>
-    </section>
+    <div>
+      <h1>Carousel com Slider do Material-UI em Next.js</h1>
+      <Swiper slidesPerView={1} pagination={{ clickable: true }} navigation>
+        {conteudo.map((depos) => (
+          <SwiperSlide key={depos.id}>
+            <h1>{depos.nome}</h1>
+            <h2>{depos.cargo}</h2>
+            <Image src={imgVoluntario} alt="teste" />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
   )
 }
